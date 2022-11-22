6c2da4a5f6944516ff2797b7afa8c0a2d81adcf4
Thế Nghĩa <105374517+thenghia2206@users.noreply.github.com>
2022-11-22 14:25:37 +0700

    Update database.py

--- database.py
+++ database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://lxdbcopgewquwd:1c862ff43f1cba25c4830149c6660662b1d530dac22509e511412ad7c258a4ef@ec2-52-4-87-74.compute-1.amazonaws.com:5432/dakrhdekn59o85"
-#mysql+mysqldb://root:22062001@127.0.0.1:3306/sensor
-#postgresql://lxdbcopgewquwd:1c862ff43f1cba25c4830149c6660662b1d530dac22509e511412ad7c258a4ef@ec2-52-4-87-74.compute-1.amazonaws.com:5432/dakrhdekn59o85
+SQLALCHEMY_DATABASE_URL = ""
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
